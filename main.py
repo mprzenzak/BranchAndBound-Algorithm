b9a912200f261f4454fc67cef4b9f8c9ab427f44
Mikołaj Przenzak <mprzenzak@it-dev.pl>
2022-11-17 20:16:21 +0100

    Working greedy low cost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import copy
 from itertools import permutations, groupby
 import time
-import numpy as np
 
 
 # Load matrix of path costs from txt data file
@@ -29,105 +28,85 @@ def get_permutation(start_node, number_of_cities):
     return permutations(nodes)
 
 
-def ignore_routes(graph, start_node, end_node):
+# Replaces costs with -1 value
+def ignore_routes(graph, start_node, end_node, path):
     for line_index in range(len(graph)):
         if line_index == start_node:
             for node_index in range(len(graph[line_index])):
                 graph[line_index][node_index] = -1
         graph[line_index][end_node] = -1
     graph[end_node][start_node] = -1
+
+    # Remove costs of routes connecting previously visited nodes with current one
+    for node in path:
+        graph[end_node][node] = -1
     return graph
 
 
-def explore_graph_level(data, number_of_cities, initial_cost, start_node, nodes_to_visit):
-    number_of_cities = int(number_of_cities)
+def explore_graph_level(data, initial_cost, start_node, nodes_to_visit, path, should_count_path_to_start_node):
     graph, cost = optimize_graph(data)
-    # nodes_to_visit = []
     costs_list = []
-    nodes_list = []
     graphs_copies = []
 
-    # for node in range(len(data[0])):
-    #     nodes_to_visit.append(node)
-    # nodes_to_visit.remove(0)
-    nodes_to_visit.remove(start_node)
-
-    # for node in range(1, number_of_cities):
-    # TODO ostatnia zmiana
     for node in nodes_to_visit:
         copy_of_data = copy.deepcopy(data)
-        copy_of_graph = ignore_routes(copy_of_data, start_node, node)
-        # to jest z 4, teraz trzeba nalozyc ograniczenia i uzyskac to co w 2 rzedzie
-        # 4 wiersz i 2 kolumnne trzeba wywalic
+        copy_of_graph = ignore_routes(copy_of_data, start_node, node, path)
         copy_of_graph, cost_of_graph = optimize_graph(copy_of_graph)
-        a = graph[start_node][node]
-        b = initial_cost
-        c = cost_of_graph
-        current_cost = a + b + c
+
+        current_cost = initial_cost + cost_of_graph
+        if graph[start_node][node] != -1:
+            current_cost += graph[start_node][node]
+
         costs_list.append(current_cost)
-        nodes_list.append(node)
         graphs_copies.append(copy_of_graph)
-    lowest_cost_node_to_continue = nodes_list[costs_list.index(min(costs_list)) - 1]
-    # TODO
-    a = lowest_cost_node_to_continue
-    return lowest_cost_node_to_continue, graphs_copies[lowest_cost_node_to_continue], min(costs_list)
+    lowest_cost_node_index = costs_list.index(min(costs_list))
+    path.append(nodes_to_visit[lowest_cost_node_index])
+
+    if should_count_path_to_start_node:
+        current_cost += data[0][nodes_to_visit[0]]
+        path.append(0)
+
+    nodes_to_visit.pop(lowest_cost_node_index)
+
+    return lowest_cost_node_index, graphs_copies[lowest_cost_node_index], min(
+        costs_list), nodes_to_visit, path, current_cost
 
 
-def find_path(data, number_of_cities, initial_cost):
-    graph_to_continue = []
+def find_path(data, initial_cost):
+    path = []
+    should_count_path_to_start_node = False
+    # Always start from 0
     start_node = 0
+    path.append(0)
 
     nodes_to_visit = []
     for node in range(len(data[0])):
         nodes_to_visit.append(node)
 
+    # Remove first node because we always start from the first
+    nodes_to_visit.remove(0)
 
+    # Find low cost node in first graph level
+    start_node, graph_to_continue, lowest_cost, nodes_to_visit, path, total_cost = explore_graph_level(data,
+                                                                                                       initial_cost,
+                                                                                                       start_node,
+                                                                                                       nodes_to_visit,
+                                                                                                       path,
+                                                                                                       should_count_path_to_start_node)
 
-    # find low cost node in first graph level
-    node_index_to_continue, graph_to_continue, lowest_cost = explore_graph_level(data, number_of_cities, initial_cost,
-                                                                                 start_node, nodes_to_visit)
-
-    next_node_to_check, graph_to_continue, lowest_cost = explore_graph_level(graph_to_continue, number_of_cities,
-                                                                             lowest_cost, node_index_to_continue + 1,
-                                                                             nodes_to_visit)
-    while node_index_to_continue != 0:  # TODO nie wiem czy taki warunek
-        next_node_to_check, graph_to_continue, lowest_cost = explore_graph_level(graph_to_continue, number_of_cities,
-                                                                                 lowest_cost, start_node + 1,
-                                                                                 nodes_to_visit)
+    while len(nodes_to_visit) > 0:
+        if len(nodes_to_visit) == 1:
+            should_count_path_to_start_node = True
+        next_node_to_check, graph_to_continue, lowest_cost, nodes_to_visit, path, total_cost = explore_graph_level(
+            graph_to_continue,
+            lowest_cost,
+            start_node + 1,
+            nodes_to_visit,
+            path,
+            should_count_path_to_start_node)
         start_node = next_node_to_check
 
-    # # Initialize min_path as maximal int size
-    # min_path = 2147483647
-    # # Initialize path as list for vertices
-    # path = []
-    # # Assume that we always start from the first node
-    # options = get_permutation(0, number_of_cities)
-    # for route in options:
-    #     weight = 0
-    #     i = 0
-    #     for vertex in route:
-    #         weight += int(data[i][vertex])
-    #         i = vertex
-    #     # Assume that we always start from the first node
-    #     weight += int(data[i][0])
-    #     if weight < min_path:
-    #         min_path = weight
-    #         path = list(route)
-    #         path.insert(0, 0)
-    #         path.append(0)
-    #############################################################################
-    # DF = DepthFirstSearch(graph, number_of_cities)
-    #
-    # # start = time.time()
-    # DF.DepthFirsIterative(0)
-    # # end = time.time()
-    # #
-    # # print('\nCzas egzekucji: ', end - start, 'segundos.\n')
-    #
-    # path = showPath(DF)
-    # print('\nsposób rozwiązanie: ', path)
-    # print('\nCosto total: ', DF.lowerCost)
-    return 0, "0"
+    return lowest_cost, path
 
 
 def load_initialization_file(filename):
@@ -169,7 +148,6 @@ def optimize_graph(data):
             for line in graph:
                 if line[i] != -1:
                     line[i] -= lowest_column_cost
-    # print(graph)
     return graph, cost_of_graph
 
 
@@ -183,26 +161,28 @@ def test_algorithm(configuration):
         min_path_weight = int(testing_case_data[2])
         min_path = list(map(int, configuration[testing_case].split("[")[1].split(']')[0].split()))
 
-        # TODO
-        if file_name == "tsp_6_1.txt" or file_name == "tsp_6_2.txt" or file_name == "hindus.txt":
+        if file_name == "tsp_6_1.txt" or file_name == "tsp_6_2.txt":
             for test in range(number_of_tests):
                 ten_repetitions_time = 0
                 for repetition in range(20):
                     start = time.time_ns()
 
                     optimized_graph, cost_of_graph = optimize_graph(load_graph("TestData/" + file_name))
-                    counted_min_path_weight, counted_path = find_path(optimized_graph,
-                                                                      get_number_of_cities("TestData/" + file_name),
-                                                                      cost_of_graph)
+                    counted_min_path_weight, counted_path = find_path(optimized_graph, cost_of_graph)
                     if counted_min_path_weight == min_path_weight:
                         print("Good job, that's right.")
                     else:
-                        print("The algorithm is wrong. Check the minimal path weight.")
+                        # print("The algorithm is wrong. Check the minimal path weight.")
+                        pass
 
                     if counted_path == min_path:
                         print("Good job, that's right.")
                     else:
                         print("The algorithm is wrong. Check the minimal path")
+                    print("Counted path:")
+                    print(counted_path)
+                    print("Counted cost:")
+                    print(counted_min_path_weight)
 
                     end = time.time_ns()
                     ten_repetitions_time += end - start
@@ -214,9 +194,7 @@ def test_algorithm(configuration):
                 start = time.time_ns()
 
                 optimized_graph, cost_of_graph = optimize_graph(load_graph("TestData/" + file_name))
-                counted_min_path_weight, counted_path = find_path(optimized_graph,
-                                                                  get_number_of_cities("TestData/" + file_name),
-                                                                  cost_of_graph)
+                counted_min_path_weight, counted_path = find_path(optimized_graph, cost_of_graph)
 
                 if counted_min_path_weight == min_path_weight:
                     print("Good job, that's right.")
@@ -227,22 +205,16 @@ def test_algorithm(configuration):
                     print("Good job, that's right.")
                 else:
                     print("The algorithm is wrong. Check the minimal path")
+                print("Counted path:")
+                print(counted_path)
+                print("Counted cost:")
+                print(counted_min_path_weight)
 
                 end = time.time_ns()
                 operation_time += end - start
                 output_file.write(str(operation_time) + "\n")
     output_file.write(configuration[len(configuration) - 1])
     output_file.close()
-
-
-# def showPath(depthFirstObj):
-#     pathStr = '[0]'
-#     bestSolution = depthFirstObj.solutionsArr[depthFirstObj.bestSolution]
-#     for i in range(0, len(bestSolution) - 1):
-#         row = bestSolution[i]
-#         col = bestSolution[i + 1]
-#         pathStr = pathStr + ' -> ' + str(depthFirstObj.graph[row, col]) + ' -> [' + str(bestSolution[i + 1]) + ']'
-#     return pathStr
 
 
 if __name__ == "__main__":
